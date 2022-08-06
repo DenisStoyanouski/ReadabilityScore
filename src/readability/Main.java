29d41587ff225bf1722e4e8e87fc2b6c774deb93
Denis Stoyanouski <vandenst@tut.by>
2022-08-06 18:22:39 +0300

    add getInput method for reading data from console;

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -1,7 +1,20 @@
 package readability;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello world!");
     }
+
+    private static String getInput() throws IOException {
+        String input = "";
+        try (Scanner scanner = new Scanner(System.in)) {
+            input = scanner.nextLine();
+        }
+        return input;
+    }
+
+
 }
