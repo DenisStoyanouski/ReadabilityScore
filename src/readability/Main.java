f2b4bb9a4a517fb625bb497cde78ce856a34bbca
Denis Stoyanouski <vandenst@tut.by>
2022-08-06 18:29:40 +0300

    add final variable of number of symbols;

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    static final int NUMBER_OF_SYMBOLS = 100;
     public static void main(String[] args) throws IOException {
         isConsidered(getInput());
     }
@@ -17,7 +18,7 @@ public class Main {
     }
 
     private static void isConsidered(String input) {
-        if (input.length() > 100) {
+        if (input.length() > NUMBER_OF_SYMBOLS) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
