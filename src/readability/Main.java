9b1321a635991789a7e22c21c44f69c97c96d768
Denis Stoyanouski <vandenst@tut.by>
2022-08-07 11:04:48 +0300

    Add methods isReadable, countAmountOfWords and countAmountOfSentences (Suppose that if the text contains sentences that on average have more than 10 words per sentence, this text is hard to read. Otherwise, this text is easy to read.)

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Main {
     static final int NUMBER_OF_SYMBOLS = 100;
     public static void main(String[] args) throws IOException {
-        isConsidered(getInput());
+        String line = getInput();
+        isReadable(countAmountOfWords(line), countAmountOfSentences(line));
     }
 
     private static String getInput() throws IOException {
@@ -19,6 +20,26 @@ public class Main {
 
     private static void isConsidered(String input) {
         if (input.length() > NUMBER_OF_SYMBOLS) {
+            System.out.println("HARD");
+        } else {
+            System.out.println("EASY");
+        }
+    }
+
+    private static int countAmountOfSentences(String input) {
+        var sentences = input.split("[.?!]");
+        return sentences.length;
+    }
+
+    private static int countAmountOfWords(String input) {
+        var words = input.split("\\s+");
+        return words.length;
+    }
+
+    private static void isReadable(int amountOfWords, int amountOfSentences) {
+
+        double readable = (double) amountOfWords / amountOfSentences;
+        if (readable > 10) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
