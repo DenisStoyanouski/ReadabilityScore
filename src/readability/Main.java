177ea8fe8299a62c2aae3cf3d0345dab2badab02
Denis Stoyanouski <vandenst@tut.by>
2022-08-07 17:48:37 +0300

    add methods for read file and count all parameters

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -1,21 +1,23 @@
 package readability;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
+    private static int words = 0;
+    private static int sentences = 0;
+    private static int characters = 0;
+
     static final int NUMBER_OF_SYMBOLS = 100;
     public static void main(String[] args) throws IOException {
-        String line = getInput();
-        isReadable(countAmountOfWords(line), countAmountOfSentences(line));
+        String line = getInput(args[0]);
+        printResult(line, getScore(line));
+
     }
 
-    private static String getInput() throws IOException {
-        String input = "";
-        try (Scanner scanner = new Scanner(System.in)) {
-            input = scanner.nextLine();
-        }
-        return input;
+    private static String getInput(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
     }
 
     private static void isConsidered(String input) {
@@ -26,25 +28,78 @@ public class Main {
         }
     }
 
-    private static int countAmountOfSentences(String input) {
-        var sentences = input.split("[.?!]");
-        return sentences.length;
+    private static void countSentences(String input) {
+        var line = input.split("[.?!]");
+        sentences =  line.length;
     }
 
-    private static int countAmountOfWords(String input) {
-        var words = input.split("\\s+");
-        return words.length;
+    private static void countWords(String input) {
+        var line = input.split("\\s+");
+        words =  line.length;
     }
 
-    private static void isReadable(int amountOfWords, int amountOfSentences) {
+    private static void countCharacters(String input) {
+        String line = input.replaceAll("[\\s|\\n|\\t]", "");
+        characters = line.length();
+    }
 
-        double readable = (double) amountOfWords / amountOfSentences;
-        if (readable > 10) {
-            System.out.println("HARD");
-        } else {
-            System.out.println("EASY");
+    private static float getScore(String line) {
+        float readable;
+        countSentences(line);
+        countWords(line);
+        countCharacters(line);
+
+        readable =  4.71f * characters / words + 0.5f * words/sentences - 21.43f;
+
+        return readable;
+    }
+
+    private static void printResult(String line, float score) {
+        System.out.println("The text is:");
+        System.out.println(line);
+        System.out.printf("Words: %d%n", words);
+        System.out.printf("Sentences: %d%n", sentences);
+        System.out.printf("Characters: %d%n", characters);
+        System.out.printf("The score is: %f%n", score);
+        System.out.printf("This text should be understood by %s-year-olds", getRage(score));
+    }
+
+    private static String getRage(float score) {
+        String rage = "";
+        switch ((int) Math.ceil(score)) {
+            case 1: rage = "5-6";
+            break;
+            case 2 : rage = "6-7";
+            break;
+            case 3 : rage = "7-8";
+            break;
+            case 4 : rage = "8-9";
+            break;
+            case 5 : rage = "9-10";
+            break;
+            case 6 : rage = "10-11";
+                break;
+            case 7 : rage = "11-12";
+                break;
+            case 8 : rage = "12-13";
+                break;
+            case 9 : rage = "13-14";
+                break;
+            case 10 : rage = "14-15";
+                break;
+            case 11 : rage = "15-16";
+                break;
+            case 12 : rage = "16-17";
+                break;
+            case 13 : rage = "17-18";
+                break;
+            case 14 : rage = "18-19";
+                break;
+            default: break;
         }
+        return rage;
     }
+
 
 
 }
